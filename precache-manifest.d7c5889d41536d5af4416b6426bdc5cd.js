8bfe1c72eddf1ca041ab3b867eb93d63f855f25a
ArnyMac <max@MacBook-Air-ArnyMac.local>
2019-09-26 13:25:55 +0300

    Updates

--- precache-manifest.d7c5889d41536d5af4416b6426bdc5cd.js
+++ precache-manifest.d7c5889d41536d5af4416b6426bdc5cd.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "846a192574418b197737554b48685981",
+    "revision": "db2a5fffd89480682426925bdf7827d1",
     "url": "/react_todo-app/index.html"
   },
   {
-    "revision": "9e0d7b0aed66ed61933b",
+    "revision": "7549d43e4df46091c972",
     "url": "/react_todo-app/static/css/main.a8e92d56.chunk.css"
   },
   {
-    "revision": "2315f74ce3c99316133d",
-    "url": "/react_todo-app/static/js/2.3310f33a.chunk.js"
+    "revision": "2abde2d1f2be2a4dd179",
+    "url": "/react_todo-app/static/js/2.1c8aca56.chunk.js"
   },
   {
-    "revision": "9e0d7b0aed66ed61933b",
-    "url": "/react_todo-app/static/js/main.b6ea25b8.chunk.js"
+    "revision": "7549d43e4df46091c972",
+    "url": "/react_todo-app/static/js/main.203b1298.chunk.js"
   },
   {
     "revision": "51af3b3c9c5f8ccb1c53",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_todo-app/precache-manifest.36c0f0e73e4ba58e6c71741bdc35771c.js"
+  "/react_todo-app/precache-manifest.d7c5889d41536d5af4416b6426bdc5cd.js"
 );
 
 self.addEventListener('message', (event) => {
